76404158bc4e187df2bb1ab0583c7927994e2716
MoonPillGyeom <ansvkf0820@naver.com>
2025-04-17 00:25:57 +0900

    setting: next.js 세팅, tailwind custom color 진행중

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <div className="w-60 bg-blue-40">test</div>
+      <div className="w-60 bg-red-50">test2</div>
+      <div className="w-60 bg-red-20">test2</div>
+      <div className="w-60 bg-black-0">test2</div>
+    </>
+  );
+}
