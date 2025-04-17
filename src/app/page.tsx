f64d42ff01cd3c85630ecee8db73b51964113154
MoonPillGyeom <ansvkf0820@naver.com>
2025-04-17 21:31:31 +0900

    feat: tailwind 색상 세팅, Input 컴포넌트 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,55 @@
+"use client";
+
+import Input, { PasswordInput } from "@/components/common/Input";
+
 export default function Home() {
+  const handleClick = () => {
+    console.log("test!!!");
+  };
   return (
     <>
-      <div className="w-60 bg-blue-40">test</div>
-      <div className="w-60 bg-red-50">test2</div>
-      <div className="w-60 bg-red-20">test2</div>
-      <div className="w-60 bg-black-0">test2</div>
+      {/* 기본 */}
+      <Input
+        id="email"
+        label="이메일"
+        placeholder="이메일을 입력해 주세요."
+        onChange={handleClick}
+        className="focus:border-blue-20"
+      />
+      {/* 폼 제출후 오류인 경우 */}
+      <Input
+        id="email"
+        label="이메일"
+        placeholder="이메일을 입력해 주세요."
+        error="잘못된 이메일 입니다."
+        onChange={handleClick}
+        className="border-red-40"
+      />
+      {/* 기본 */}
+      <PasswordInput
+        id="password"
+        label="비밀번호"
+        placeholder="비밀번호를 입력해 주세요"
+        onChange={handleClick}
+        className="focus:border-blue-20"
+      />
+      {/* 폼 제출후 오류인 경우 */}
+      <PasswordInput
+        id="password"
+        label="비밀번호"
+        placeholder="비밀번호를 입력해 주세요"
+        onChange={handleClick}
+        error="최소 8자 이상"
+        className="border-red-40"
+      />
+      {/* 기본 */}
+      <PasswordInput
+        id="password"
+        label="비밀번호 확인"
+        placeholder="비밀번호를 한번 더 입력해 주세요"
+        onChange={handleClick}
+        className="focus:border-blue-20"
+      />
     </>
   );
 }
---
+++ src/components/common/Input.tsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+
+interface InputProps {
+  id: string;
+  label?: string;
+  type?: string;
+  placeholder: string;
+  value?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  error?: string;
+  className?: string;
+}
+
+/**
+ * @param id htmlFor,id
+ */
+function Input({
+  id,
+  label,
+  type = "text",
+  placeholder,
+  value,
+  onChange,
+  error,
+  className,
+  ...props
+}: InputProps) {
+  return (
+    <div className="flex flex-col gap-2.5">
+      {label && (
+        <label htmlFor={id} className="text-white text-lg">
+          {label}
+        </label>
+      )}
+      <input
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className={`text-white text-md w-full px-6 py-5 border border-gray-40 focus:outline-none rounded-md shadow-sm bg-black-20 ${className}`}
+        {...props}
+      />
+      {error && <p className="text-red-40">{error}</p>}
+    </div>
+  );
+}
+
+export function PasswordInput({
+  id,
+  label,
+  placeholder,
+  value,
+  onChange,
+  error,
+  className,
+  ...props
+}: InputProps) {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
+
+  return (
+    <div className="flex flex-col gap-2.5">
+      {label && (
+        <label htmlFor={id} className="text-white text-lg">
+          {label}
+        </label>
+      )}
+      <div className="relative">
+        <input
+          id={id}
+          type={isPasswordVisible ? "text" : "password"}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          className={`text-white text-md w-full px-6 py-5 border border-gray-40 focus:outline-none rounded-md shadow-sm bg-black-20 ${className}`}
+          {...props}
+        />
+        <button
+          type="button"
+          onClick={togglePasswordVisibility}
+          className="absolute top-1/2 right-6 transform -translate-y-1/2 text-white"
+        >
+          {isPasswordVisible ? (
+            <span role="img" aria-label="hide">
+              🙈
+            </span>
+          ) : (
+            <span role="img" aria-label="show">
+              👁️
+            </span>
+          )}
+        </button>
+      </div>
+      {error ? (
+        <p className="text-red-40">{error}</p>
+      ) : (
+        <p className="text-gray-40">최소 8자 이상</p>
+      )}
+    </div>
+  );
+}
+
+export default Input;
